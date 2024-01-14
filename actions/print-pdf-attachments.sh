3f31d1132ea499ac4372dfa1f9e25a5abdd71699
Jan Willamowius <jan@willamowius.de>
2024-01-14 15:38:10 +0100

    allow 2nd optional parameter to set more printing options, eg "-o sides=two-sided-long-edge" for double-sided

--- actions/print-pdf-attachments.sh
+++ actions/print-pdf-attachments.sh
@@ -3,13 +3,14 @@ SUPPORTED_FILETYPES=.pdf
 LP_OPTIONS="-o media=A4,tray1 -o fit-to-page -o position=top -o scaling=100"
 
 FILENAME=$1
+LP_EXTRA_OPTIONS=$2
 
 if [ "$?" = "0" ]; then
 	/usr/bin/uudeview +e $SUPPORTED_FILETYPES -p ~/print_tmp/ -i -q $FILENAME
 	for f in ~/print_tmp/*
 	do
 		test -f "$f" || continue
-		lp $LP_OPTIONS "$f" > /dev/null
+		lp $LP_OPTIONS $LP_EXTRA_OPTIONS "$f" > /dev/null
 		rm "$f"
 	done
 fi
